a1fbfbf69b05dd28fb004998ca843b6298e96370
Abu Syiam <siyam.alorferi@gmail.com>
2025-05-14 19:45:09 +0600

    run succesfully in ios

--- composeApp/src/commonMain/kotlin/com/example/youtube_video_player/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/youtube_video_player/App.kt
@@ -3,6 +3,7 @@ package com.example.youtube_video_player
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -10,6 +11,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import chaintech.videoplayer.host.MediaPlayerHost
+import chaintech.videoplayer.model.VideoPlayerConfig
+import chaintech.videoplayer.ui.youtube.YouTubePlayerComposable
+import chaintech.videoplayer.util.ComposeResourceDrawable
 import com.example.youtube_video_player.presentation.MainScreen
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -20,12 +27,40 @@ import youtubevideoplayer.composeapp.generated.resources.compose_multiplatform
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+
+    val playerHost = remember { MediaPlayerHost(mediaUrl = "QFxN2oDKk0E") }
+
+    YouTubePlayerComposable(
+        modifier = Modifier.fillMaxSize(),
+        playerHost = playerHost,
+        playerConfig = VideoPlayerConfig(
+            isPauseResumeEnabled = true,
+            isSeekBarVisible = true,
+            isDurationVisible = true,
+            seekBarThumbColor = Color.Red,
+            seekBarActiveTrackColor = Color.Red,
+            seekBarInactiveTrackColor = Color.White,
+            durationTextColor = Color.White,
+            seekBarBottomPadding = 10.dp,
+            pauseResumeIconSize = 40.dp,
+            isAutoHideControlEnabled = true,
+            controlHideIntervalSeconds = 5,
+            isFastForwardBackwardEnabled = true,
+        )
+    )
+
+
+
+
+
+
+
+    /*MaterialTheme {
 
         MainScreen()
 
 
-        /*var showContent by remember { mutableStateOf(false) }
+        *//*var showContent by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = { showContent = !showContent }) {
                 Text("Click me!")
@@ -37,6 +72,6 @@ fun App() {
                     Text("Compose: $greeting")
                 }
             }
-        }*/
-    }
+        }*//*
+    }*/
 }
